15ba50f43faa3aee1168d7ee7589622040c72585
root <root@localhost>
2022-04-20 00:31:25 +0600

    auto publish 1650393084588

---
+++ src/while_loop.py
@@ -0,0 +1,33 @@
+
+#declare a var is need for while loop
+
+count = 0
+
+while count <= 10:
+  print("Count",count)
+  count = count + 1
+
+
+counter = 10
+
+while counter > 0:
+  print("play",counter)
+  counter = counter - 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
