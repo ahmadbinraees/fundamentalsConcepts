a1f900f211f254961b40a8f53ae0750e5a7373ed
root <root@localhost>
2022-04-22 14:44:55 +0600

    auto publish 1650617094764

---
+++ src/function.py
@@ -0,0 +1,43 @@
+#this is a simple function
+def blend(num):
+  print(num+2)
+
+blend(2)
+#this function is working
+
+# plus function
+def plus_nums(plus_num1 , plus_num2):
+  sum = plus_num1 + plus_num2
+  print(sum)
+
+plus_nums(2 , 2)
+
+
+# minus function
+def minus_nums(minus_digit1 , minus_digit2):
+  minus_sum = minus_digit1 - minus_digit2
+  print(minus_sum)
+
+minus_nums(6 , 2)
+
+
+# multiply function
+def multiply_nums(mult_digit1 , mult_digit2):
+  multiply_sum = mult_digit1 * mult_digit2
+  print(multiply_sum)
+
+multiply_nums(2 , 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
