81b361940db39735b0eeb502b3e5bbec0b060b15
root <root@localhost>
2022-04-23 10:48:11 +0600

    auto publish 1650689291102

--- src/for_loop.py
+++ src/for_loop.py
@@ -1,17 +1,25 @@
-nums = [12,22,32,42,52,62,72]
+#nums = [12,22,32,42,52,62,72]
 
-for num in nums:
-  print(num)
-
-
-fruit_item = ["Apple","Banana","Jack Fruit","Lichi","Orsnge"]
-
-for count in fruit_item:
-  title = count + " Fruit"
-  print(title)
+#for num in nums:
+  #print(num)
 
 
+#fruit_item = ["Apple","Banana","Jack Fruit","Lichi","Orsnge"]
 
+#for count in fruit_item:
+#  title = count + " Fruit"
+  #print(title)
+
+
+
+#making a breakable logic for loop
+
+mobile_names = ["SM","SE","NK","LG","MI"]
+for cell in mobile_names:
+  if cell == "LG":
+   continue
+   print(cell)
+  
 
 
 
