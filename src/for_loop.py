dc8ecacf50714a28d85118877d41c21767d52ef3
root <root@localhost>
2022-04-23 10:57:39 +0600

    auto publish 1650689859091

--- src/for_loop.py
+++ src/for_loop.py
@@ -18,25 +18,26 @@ mobile_names = ["SM","SE","NK","LG","MI"]
 
 for cell in mobile_names:
   if cell == "LG":
-   continue
-   print(cell)
+   break
+  print(cell)
   
 
 
 # making a skiping logic
 
 for fon in mobile_names:
-  if fon == "NK":
+   if fon == "NK":
     continue
-    print(fon)
+   print(fon)
   
 for op in nums:
-  if op < 0:
+   if op < 0:
     continue
-    print(op)
+   print(op)
   
 
 # codes not working. i cant find out the bug
+# the bug was fixed. it was the indentetion error
 
 
 
